24be129fb707dc7e9fcf2b6360b114e3eda2d2ef
PierluigiPapa <pierluigipapa9@gmail.com>
2024-02-07 20:23:21 +0100

    fix: adjust footer

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -13,6 +13,20 @@ export default {
 </script>
 
 <template>
+    <div class="container">
+        <div class="content">
+            <div class="menu">
+                <MenuFooter></MenuFooter>
+            </div>
+
+            <div class="logo">
+                <LogoFooter></LogoFooter>
+            </div>
+        </div>
+        <div class="contacts">
+            <EndFooter></EndFooter>
+        </div>
+    </div>
     
 </template>
   
--- src/components/footer/LogoFooter.vue
+++ src/components/footer/LogoFooter.vue
@@ -1,20 +1,7 @@
 <script></script>
 
 <template>
-    <div class="container">
-        <div class="content">
-            <div class="menu">
-                <MenuFooter></MenuFooter>
-            </div>
-
-            <div class="logo">
-                <LogoFooter></LogoFooter>
-            </div>
-        </div>
-        <div class="contacts">
-            <EndFooter></EndFooter>
-        </div>
-    </div>
+    
 </template>
 
 <style lang="scss">
