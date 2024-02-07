881966af92500c3de100572d12554c6be05118ae
PierluigiPapa <pierluigipapa9@gmail.com>
2024-02-07 16:42:00 +0100

    feat: add Header

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,6 @@ export default {
 
 </template>
 
-<style>
-
+<style lang="scss">
+@use "./styles/general.scss";
 </style>
--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,19 +1,115 @@
 <script>
 export default {
   name: 'Header',
+  data () {
+    return {
+        navLi: [
+        {
+        text: "CHARACTERS",
+        link: "#",
+        current: false
+    },
+    {
+        text: "COMICS",
+        link: "#",
+        current: true
+    },
+    {
+        text: "MOVIES",
+        link: "#",
+        current: false
+    },
+    {
+        text: "TV",
+        link: "#",
+        current: false
+    },
+    {
+        text: "GAMES",
+        link: "#",
+        current: false
+    },
+    {
+        text: "COLLECTIBLES",
+        link: "#",
+        current: false
+    },
+    {
+        text: "VIDEOS",
+        link: "#",
+        current: false
+    },
+    {
+        text: "FANS",
+        link: "#",
+        current: false
+    },
+    {
+        text: "NEWS",
+        link: "#",
+        current: false
+    },
+    {
+        text: "SHOP",
+        link: "#",
+        current: false
+    }
+]
+    }
   }
+};
 </script>
 
 <template>
-
-<h1>La mia prima app con Vite!</h1>
-
+     <header>
+        <div class="container">
+            <img src="/img/dc-logo.png" alt="Logo_DC_Comics">
+            <ul>
+                <li v-for="(item, index) in navLi" :key="index" :class="{active : item.current}"><a href="item.link">{{item.text}}</a></li>
+            </ul>
+        </div>
+    </header>
 </template>
 
-<style scoped>
+<style lang="scss"scoped>
+@use '../../styles/partials/mixins' as *;
 
-h1 {
-    color: red;
+header {
+    background-color: #fff;
+    font-size: 15px;
+    padding-top: 25px;
+
+    .container {
+        height: 100px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    img {
+        height: 70%;
+        padding-left: 150px;
+    }
+    ul {
+        flex-grow: 1;
+        text-align: right;
+        line-height: 95px;
+        padding-right: 70px;
+
+        li {
+            display: inline-block;
+            font-weight: bold;
+            color: $grey;
+            margin: 0 10px;
+            &:hover {
+                cursor: pointer;
+                color: $blue;
+            }
+        }
+        .active {
+            color: $blue;
+            border-bottom: 5px solid $blue;
+        }
+    }
 }
 
 </style>
