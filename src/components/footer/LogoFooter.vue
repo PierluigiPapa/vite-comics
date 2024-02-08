04fa279b4eaada6920c3b34947eefe8c17d5630d
PierluigiPapa <pierluigipapa9@gmail.com>
2024-02-08 13:31:57 +0100

    fix: padding-right on .logo-footer

--- src/components/footer/LogoFooter.vue
+++ src/components/footer/LogoFooter.vue
@@ -3,11 +3,15 @@
 
 <template>
    <div>
-        <img src="/img/dc-logo-bg.png" alt="Logo_DC_Comics">
+        <img class="logo-footer" src="/img/dc-logo-bg.png" alt="Logo_DC_Comics">
     </div> 
 </template>
 
 <style lang="scss">
 @use '../../styles/partials/variables.scss' as *;
+
+.logo-footer {
+    padding-right: 100px;
+}
 
 </style>
